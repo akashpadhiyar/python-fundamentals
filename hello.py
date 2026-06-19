272a6ae843b92ad14c51725d06b1306032d6ce73
Akash Padhiyar <akashpadhiyar@users.noreply.github.com>
2026-06-19 16:35:39 +0530

    First Commit
    First Commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+print("Hello World")
+#Single Line Comment 
+'''
+Line 1
+Line2 
+'''
+"""Line 1 
+Line 2
+"""
+
+print("Hello World 2")
---
+++ sumdemo.py
@@ -0,0 +1,14 @@
+a = int(input("Enter No1:"))
+b = int(input("Enter No2:"))
+c = a + b
+#method 1
+print("A Value is ",a)
+print("B Value is ",b)
+print("C Value is ",c)
+#method 2 
+print("\nA Value is ",a,"\nB Value is ",b,"\nC Value is ",c)
+#method 3 
+print("\nA Value is {}\nB Value is {}\nC Value is {}".format(a,b,c))
+#method 4 
+print(f"\nA Value is {a}\nB Value is {b}\nC Value is {c}")
+
