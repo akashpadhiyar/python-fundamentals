7d73fc13590a1c8e3eab5dcd3cde322fe895c35e
Akash Padhiyar <akashpadhiyar@users.noreply.github.com>
2026-06-19 16:40:25 +0530

    V1.1
    CM3

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-print("Hello World")
+print("Hello World in Python")
 #Single Line Comment 
 '''
 Line 1
@@ -9,3 +9,6 @@ Line 2
 """
 
 print("Hello World 2")
+
+print("\n Hello World in New Line")
+print("\nNew Line Example")
